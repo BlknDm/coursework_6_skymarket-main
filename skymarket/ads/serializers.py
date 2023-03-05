67d0a40b5193e3c28847c9a7b75686f649eb2b71
BlknDm <89090018300@mail.ru>
2023-03-06 00:12:42 +0300

    coursework_6 init

---
+++ skymarket/ads/serializers.py
@@ -0,0 +1,46 @@
+from rest_framework import serializers
+
+from .models import Ad, Comment
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    author_id = serializers.IntegerField(source='author.id', read_only=True)
+    ad_id = serializers.IntegerField(source='ad.id', read_only=True)
+    author_first_name = serializers.CharField(source='author.first_name', read_only=True)
+    author_last_name = serializers.CharField(source='author.last_name', read_only=True)
+    author_image = serializers.ImageField(source='author.image', read_only=True)
+
+    class Meta:
+        model = Comment
+        fields = ('pk',
+                  'text',
+                  'author_id',
+                  'author_first_name',
+                  'author_last_name',
+                  'author_image',
+                  )
+
+
+class AdSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Ad
+        fields = ('pk', 'title', 'price', 'author')
+
+
+class AdDetailSerializer(serializers.ModelSerializer):
+    author_first_name = serializers.SerializerMethodField()
+    author_last_name = serializers.SerializerMethodField()
+    author_id = serializers.SerializerMethodField()
+
+    def get_author_first_name(self, obj):
+        return obj.author.first_name
+
+    def get_author_last_name(self, obj):
+        return obj.author.last_name
+
+    def get_author_id(self, obj):
+        return obj.author.id
+
+    class Meta:
+        model = Ad
+        fields = ('pk', 'title', 'price', 'author', 'author_first_name')
